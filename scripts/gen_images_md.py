11116d5685915766d097c8175de5b87e2fb6444b
jiangdengke <jiang1728439852@gmail.com>
2025-07-18 23:37:57 +0800

    something

--- scripts/gen_images_md.py
+++ scripts/gen_images_md.py
@@ -7,6 +7,7 @@ md_lines = ["## 图片预览\n"]
 
 for file in sorted(os.listdir(IMAGE_DIR)):
     if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.bmp')):
+        md_lines.append(f"**{file}**")
         md_lines.append(f"![{file}]({IMAGE_DIR}/{file})")
 
 with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
