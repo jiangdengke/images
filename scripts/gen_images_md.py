09fda9616f8f8c62a225146f1dafe4138c5cc11b
jiangdengke <jiang1728439852@gmail.com>
2025-07-18 22:59:45 +0800

    first commit

---
+++ scripts/gen_images_md.py
@@ -0,0 +1,13 @@
+import os
+
+IMAGE_DIR = "images"
+OUTPUT_FILE = "images.md"
+
+md_lines = ["## 图片预览\n"]
+
+for file in sorted(os.listdir(IMAGE_DIR)):
+    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.bmp')):
+        md_lines.append(f"![{file}]({IMAGE_DIR}/{file})")
+
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    f.write("\n".join(md_lines))
